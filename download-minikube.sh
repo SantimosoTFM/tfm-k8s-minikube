f1f187b1c820f040639ef2de8a23de71f1bbb708
Santiago Mora Soler <Santiago.Mora.Soler1@ibm.com>
2019-02-20 18:48:53 +0100

    added scripts and troubleshooting txt

---
+++ download-minikube.sh
@@ -0,0 +1,3 @@
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+# v0.34.1 works fine!
---
+++ start-cluster-minikube.sh
@@ -0,0 +1,1 @@
+minikube start --vm-driver kvm2 --memory 16000 --cpus=4
